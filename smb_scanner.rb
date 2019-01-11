9ec8f4ce6e33fa2f038d376c09c47ed51ce52e2d
Bitcoin Bandit <44848162+ethereumbandit@users.noreply.github.com>
2019-01-11 11:35:29 -0500

    Update smb_scanner.rb

--- smb_scanner.rb
+++ smb_scanner.rb
@@ -1,7 +1,7 @@
 ##
 # This module requires Metasploit: http://metasploit.com/download
 # Current source: https://github.com/rapid7/metasploit-framework
-#By Subham Dasgupta 
+# Exploit by bandit
 ##
 
 class MetasploitModule < Msf::Auxiliary
